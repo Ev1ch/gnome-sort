ed6d8a02f30e6361acb7903408bcb9eaba1f74df
Vlad Zusko <vladzvu11@gmail.com>
2023-04-17 22:19:18 +0300

    feat: add data constants

--- src/constants.py
+++ src/constants.py
@@ -20,3 +20,14 @@ ALL_ALGORITHMS: List[AlgorithmVariant] = [
     ("sort", sort),
     ("sort_multithreaded", sort_multithreaded),
 ]
+
+DATA_ROOT_PATH = "D:\\University\\Parralel programming\\Course\\src\\data"
+
+DATA = {
+    1_000: DATA_ROOT_PATH + "\\1000.csv",
+    2_000: DATA_ROOT_PATH + "\\2000.csv",
+    3_000: DATA_ROOT_PATH + "\\3000.csv",
+    5_000: DATA_ROOT_PATH + "\\5000.csv",
+    8_000: DATA_ROOT_PATH + "\\8000.csv",
+    10_000: DATA_ROOT_PATH + "\\10000.csv",
+}
