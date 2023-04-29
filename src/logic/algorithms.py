b4b1d33ea3865571bb6da037375d5a50d9a73cdf
Vlad Zusko <vladzvu11@gmail.com>
2023-04-29 23:08:03 +0300

    feat: update algorithms logic

--- src/logic/algorithms.py
+++ src/logic/algorithms.py
@@ -1,12 +1,34 @@
 from typing import List
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 
-from .domain import Item, Comparer, Algorithm, MultithreadedAlgorithm, IllegalArgumentError
-from .utils import is_bigger_or_equal, is_smaller_or_equal, get_chunks_indexes_pairs
-from .multithreading import get_chunk_size, get_optimal_threads_number
+from .domain import (
+    Item,
+    Comparer,
+    Algorithm,
+    MultithreadedAlgorithm,
+    IllegalArgumentError,
+)
+from .utils import (
+    is_bigger_or_equal,
+    is_smaller_or_equal,
+    get_chunks_indexes_pairs,
+    are_futures_done,
+)
+from .multithreading import (
+    IndexedFuture,
+    get_chunk_size,
+    get_optimal_threads_number,
+    get_indexed_futures_filter,
+    get_scoped_worker,
+)
 
 
-def algorithm(collection: List[Item], comparer: Comparer[Item], start_index: int = 0, end_index: int = None):
+def algorithm(
+    collection: List[Item],
+    comparer: Comparer[Item],
+    start_index: int = 0,
+    end_index: int = None,
+):
     index = start_index
     current_end_index = end_index if end_index is not None else len(collection) - 1
 
@@ -14,9 +36,18 @@ def algorithm(collection: List[Item], comparer: Comparer[Item], start_index: int
         if index == 0 or comparer(collection[index], collection[index - 1]):
             index = index + 1
         else:
-            collection[index], collection[index -
-                                          1] = collection[index-1], collection[index]
+            collection[index], collection[index - 1] = (
+                collection[index - 1],
+                collection[index],
+            )
             index = index - 1
+
+
+def get_scoped_algorithm(collection: List[Item], comparer: Comparer[Item]):
+    def sorter(start_index: int, end_index: int):
+        algorithm(collection, comparer, start_index, end_index)
+
+    return sorter
 
 
 def gnome_sort(comparer: Comparer[Item]):
@@ -31,7 +62,7 @@ def gnome_sort(comparer: Comparer[Item]):
 def gnome_sort_multithreaded(comparer: Comparer[Item], minimum_chunk_size: int):
     if minimum_chunk_size <= 1:
         raise IllegalArgumentError("Minimum chunk size must be bigger than 1")
-    
+
     def sorter(initial_collection: List[Item], threads_number: int):
         if threads_number <= 0:
             raise IllegalArgumentError("Threads number must be positive")
@@ -41,18 +72,49 @@ def gnome_sort_multithreaded(comparer: Comparer[Item], minimum_chunk_size: int):
 
         # We include 0 and 1 to avoid slicing empty list
         if collection_length <= 1:
-            return collection.copy()
+            return collection
 
-        chunk_size = get_chunk_size(collection_length, threads_number, minimum_chunk_size)
-        chunks_indexes_pairs = get_chunks_indexes_pairs(collection, chunk_size)
+        chunk_size = get_chunk_size(
+            collection_length, threads_number, minimum_chunk_size
+        )
+        chunks_indexes_pairs = get_chunks_indexes_pairs(
+            collection, chunk_size, right_offset=1
+        )
         chunks_number = len(chunks_indexes_pairs)
-    
         executor = ThreadPoolExecutor(chunks_number)
-        for chunk_indexes_pair in chunks_indexes_pairs:
-            executor.submit(algorithm, collection, comparer, *chunk_indexes_pair)
-        executor.shutdown(wait=True)
+        indexed_futures: List[IndexedFuture] = []
+        scoped_algorithm = get_scoped_algorithm(collection, comparer)
+        are_any_indexed_futures_with_state = get_indexed_futures_filter(indexed_futures)
 
-        algorithm(collection, comparer, 0, collection_length - 1)
+        def add_worker(index: int):
+            scoped_worker = get_scoped_worker(
+                collection,
+                chunks_indexes_pairs,
+                scoped_algorithm,
+                add_worker,
+                are_any_indexed_futures_with_state,
+            )
+
+            indexed_futures.append(
+                (
+                    index,
+                    executor.submit(
+                        scoped_worker,
+                        index,
+                    ),
+                )
+            )
+
+        for index in range(chunks_number):
+            add_worker(index)
+
+        while not are_futures_done(
+            indexed_future[1] for indexed_future in indexed_futures
+        ):
+            futures = [indexed_future[1] for indexed_future in indexed_futures]
+            wait(futures)
+
+        executor.shutdown()
 
         return collection
 
@@ -82,7 +144,11 @@ sort_descending_multithreaded: MultithreadedAlgorithm = gnome_sort_multithreaded
 )
 
 
-def sort_multithreaded(collection: List[Item], threads_number = get_optimal_threads_number(), is_reversed = False):
+def sort_multithreaded(
+    collection: List[Item],
+    threads_number=get_optimal_threads_number(),
+    is_reversed=False,
+):
     if is_reversed:
         return sort_descending_multithreaded(collection, threads_number)
 
